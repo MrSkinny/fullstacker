afd27a6423504564bfddf0774190e8d2297e8f41
Rich Greenhill <rgnet1@yahoo.com>
2017-03-17 14:29:05 -0700

    with build

--- index-compiled.js
+++ index-compiled.js
@@ -1,41 +1,43 @@
 'use strict';
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
-var _Cheese = require('./models/Cheese');
+var _routes = require('./routes');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var buildPath = function buildPath() {
+    return process.env.NODE_ENV !== 'production' ? _path2.default.join(__dirname, '../build') : _path2.default.join(__dirname, 'build');
+};
+
 var app = (0, _express2.default)();
-
-var CHEESES = ["Bath Blue", "Barkham Blue", "Buxton Blue", "Cheshire Blue", "Devon Blue", "Dorset Blue Vinney", "Dovedale", "Exmoor Blue", "Harbourne Blue", "Lanark Blue", "Lymeswold", "Oxford Blue", "Shropshire Blue", "Stichelton", "Stilton", "Blue Wensleydale", "Yorkshire Blue"];
-
-app.get('/cheeses', function (req, res) {
-    res.json(CHEESES);
-});
+app.use(_express2.default.static(buildPath()));
+app.use('/cheeses', _routes.cheeseRoutes);
 
 app.listen(3001, function () {
     return console.log('Server on 3001');
 });
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.Cheese = undefined;
+exports.cheeseRoutes = undefined;
 
-var _mongoose = require('mongoose');
+var _express = require("express");
 
-var _mongoose2 = _interopRequireDefault(_mongoose);
+var CHEESES = ["Bath Blue", "Barkham Blue", "Buxton Blue", "Cheshire Blue", "Devon Blue", "Dorset Blue Vinney", "Dovedale", "Exmoor Blue", "Harbourne Blue", "Lanark Blue", "Lymeswold", "Oxford Blue", "Shropshire Blue", "Stichelton", "Stilton", "Blue Wensleydale", "Yorkshire Blue"];
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var cheeseRoutes = (0, _express.Router)();
 
-var CheeseSchema = _mongoose2.default.Schema({
-    name: String
+cheeseRoutes.get('/', function (req, res) {
+    res.json(CHEESES);
 });
 
-var Cheese = _mongoose2.default.model('cheese', CheeseSchema);
-
-exports.Cheese = Cheese;
+exports.cheeseRoutes = cheeseRoutes;
